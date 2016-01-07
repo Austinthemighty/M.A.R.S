2999b629545621b68018cd2389178316a0895fb5
Austin Chopra <austin.chopra@west-mec.org>
2016-01-06 18:12:20 -0700

    no message

--- mySQLconnet.php
+++ mySQLconnet.php
@@ -2,35 +2,23 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
-$dbname = "Mars_reservation";
+$dbname = "reservation";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
-
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully";
-// Create database
-$sql = "CREATE DATABASE myDB";
+
+$sql = "INSERT INTO reservation (id,firstName, lastLame)
+VALUES ('1', 'John', 'Doe')";
+
 if ($conn->query($sql) === TRUE) {
-    echo "Database created successfully";
+    echo "New record created successfully";
 } else {
-    // sql to create table
-    $sql = "CREATE TABLE MyGuests (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-    firstname VARCHAR(30) NOT NULL,
-    lastname VARCHAR(30) NOT NULL,
-    email VARCHAR(50),
-    reg_date TIMESTAMP
-    )";
-    
-    if ($conn->query($sql) === TRUE) {
-        echo "Table MyGuests created successfully";
-    } else {
-        echo "Error creating table: " . $conn->error;
-    }
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
+$conn->close();
 ?>
