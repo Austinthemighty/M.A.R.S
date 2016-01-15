ece79ea51c201f1c40453ba237fce6fa9d31d1f5
Austin Chopra <austin.chopra@west-mec.org>
2016-01-14 17:49:03 -0700

    fixed some titles and upload	brandon code for examples

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
 
-    <title>Gunoi</title>
+    <title>M.A.R.S</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
@@ -78,11 +78,11 @@ if(@$_POST['formSubmit'] == "Submit")
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a  class="navbar-brand" href="index.html">M.A.R.S</a>
+            <a  class="navbar-brand" href="index.php">M.A.R.S</a>
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="#contact">CONTACT</a></li>
+                <li><a href="contact.php">CONTACT</a></li>
             </ul>
         </div>
     </div>
